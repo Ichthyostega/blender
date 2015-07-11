27ab6af0c3adb13592d6ac3feb10fd477bd14316
Ichthyostega <prg@ichthyostega.de>
2015-07-11 21:34:12 +0200

    [private MM] Lege eine Mindmap an für den weiteren Review / Integrationsprozeß

---
+++ Stab2D.mm
@@ -0,0 +1,290 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#00ff66" CREATED="1436641527729" ID="ID_715731665" MODIFIED="1436641623494">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="5">Blender </font>
+    </p>
+    <p style="text-align: center">
+      <b><font size="5">Stab-2D</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1436641676560" HGAP="60" ID="ID_1218644922" MODIFIED="1436641851143" POSITION="left" TEXT="Build" VSHIFT="-61">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1436641870656" ID="ID_1917334295" MODIFIED="1436641873243" TEXT="Umgebung">
+<node CREATED="1436641905079" ID="ID_32595620" MODIFIED="1436641910011" TEXT="SCons-Build"/>
+<node CREATED="1436641912336" ID="ID_1618985498" MODIFIED="1436641915507" TEXT="Eclipse-CDT"/>
+<node CREATED="1436641916575" ID="ID_1658953336" MODIFIED="1436641921363" TEXT="Git-Zweig"/>
+</node>
+<node CREATED="1436641873568" ID="ID_38644124" MODIFIED="1436641878147" TEXT="Backport">
+<node CREATED="1436641925670" ID="ID_1080364414" MODIFIED="1436641933019" TEXT="Debian/unstable"/>
+<node CREATED="1436641933487" ID="ID_761151833" MODIFIED="1436641943273" TEXT="Debian/Wheezy">
+<node CREATED="1436641944199" ID="ID_1369329699" MODIFIED="1436641947899" TEXT="nur Python 3.2">
+<node CREATED="1436642050607" ID="ID_1468804991" MODIFIED="1436642060680" TEXT="Fehler-Diagnostik-Meldungen"/>
+<node CREATED="1436642013735" ID="ID_715643907" MODIFIED="1436642029139" TEXT="String compilieren/interpretieren"/>
+<node CREATED="1436642029567" ID="ID_1355847098" MODIFIED="1436642034659" TEXT="Unicode f&#xfc;r Code"/>
+<node CREATED="1436642037079" ID="ID_108601883" MODIFIED="1436642044682" TEXT="Name der Import-Lib"/>
+</node>
+<node CREATED="1436641950647" ID="ID_588391845" MODIFIED="1436641995509" TEXT="OpenEXR 2.2.0">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_588391845" ENDARROW="Default" ENDINCLINATION="-48;30;" ID="Arrow_ID_1192109263" SOURCE="ID_1205869713" STARTARROW="None" STARTINCLINATION="86;-8;"/>
+</node>
+</node>
+</node>
+<node CREATED="1436641880680" ID="ID_676998091" MODIFIED="1436641882932" TEXT="Upgrade">
+<node CREATED="1436641884088" ID="ID_40997503" MODIFIED="1436641897625" TEXT="2.70">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1436641888025" ID="ID_1205869713" MODIFIED="1436641995508" TEXT="2.75a">
+<arrowlink DESTINATION="ID_588391845" ENDARROW="Default" ENDINCLINATION="-48;30;" ID="Arrow_ID_1192109263" STARTARROW="None" STARTINCLINATION="86;-8;"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1436641853957" HGAP="81" ID="ID_576569203" MODIFIED="1436641863949" POSITION="left" TEXT="Review" VSHIFT="9">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1436642114382" ID="ID_975531283" MODIFIED="1436642117826" TEXT="Entry"/>
+<node CREATED="1436642124742" ID="ID_1890100547" MODIFIED="1436642133674" TEXT="erster Anlauf"/>
+<node CREATED="1436642134070" ID="ID_1101247684" MODIFIED="1436642137250" TEXT="zweiter Anlauf"/>
+</node>
+<node CREATED="1436642086687" HGAP="-16" ID="ID_667891095" MODIFIED="1436642092300" POSITION="left" TEXT="Doku" VSHIFT="95">
+<node CREATED="1436642093110" ID="ID_1489772734" MODIFIED="1436642098202" TEXT="Github-Wiki"/>
+<node CREATED="1436642098798" ID="ID_1828646356" MODIFIED="1436642101282" TEXT="Video"/>
+</node>
+<node CREATED="1436642157958" HGAP="29" ID="ID_1521617149" MODIFIED="1436643152320" POSITION="right" TEXT="Konzept" VSHIFT="-31">
+<node CREATED="1436643123858" ID="ID_1293962496" MODIFIED="1436643178230">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Werkzeug</b>&#160;statt Magie
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1436643133194" ID="ID_1822818488" MODIFIED="1436643167139" TEXT="Der Kameramann wei&#xdf; was er tut">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1436643199249" ID="ID_1161855894" MODIFIED="1436643201598" TEXT="Workflow"/>
+</node>
+<node CREATED="1436642151630" ID="ID_552228576" MODIFIED="1436642168950" POSITION="right" TEXT="Entwicklung">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1436642678428" ID="ID_1447543726" MODIFIED="1436642781150" TEXT="Grundkonzept umgesetzt">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1436642688428" ID="ID_466533085" MODIFIED="1436642701588">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Trennung von Me&#223;wert
+    </p>
+    <p>
+      und Manipulation
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1436642787204" ID="ID_1096643282" MODIFIED="1436642794408" TEXT="Koordinaten">
+<node CREATED="1436642795331" ID="ID_1403585828" MODIFIED="1436642800167" TEXT="logisch definieren"/>
+<node CREATED="1436642800523" ID="ID_281646314" MODIFIED="1436642807799" TEXT="zwischen Canvas und Frame unterscheiden"/>
+</node>
+</node>
+<node CREATED="1436642712356" ID="ID_214702345" MODIFIED="1436642774582" TEXT="Code &#xfc;berarbeitet">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1436642717260" ID="ID_1771565832" MODIFIED="1436642723896" TEXT="symmetrisch machen"/>
+<node CREATED="1436642724796" ID="ID_547887915" MODIFIED="1436642728968" TEXT="lesbarer machen"/>
+</node>
+<node CREATED="1436642732140" HGAP="39" ID="ID_1640021324" MODIFIED="1436642858625" TEXT="Automation" VSHIFT="1">
+<icon BUILTIN="prepare"/>
+<node CREATED="1436642739276" ID="ID_1785940699" MODIFIED="1436642825970" TEXT="Teil I : automatisierbar machen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1436642749748" ID="ID_762314661" MODIFIED="1436642766450" TEXT="Teil II: Automation editierbar machen">
+<icon BUILTIN="bell"/>
+</node>
+</node>
+<node CREATED="1436642834707" HGAP="43" ID="ID_1765241815" MODIFIED="1436642886796" VSHIFT="33">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Erweiterte
+    </p>
+    <p>
+      <font size="4">Funktionen</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="hourglass"/>
+<node CREATED="1436642890723" ID="ID_1737347479" MODIFIED="1436643102784" TEXT="auto-Zoom">
+<icon BUILTIN="smiley-neutral"/>
+</node>
+<node CREATED="1436642904227" ID="ID_673570813" MODIFIED="1436642913113" TEXT="Rahmen f&#xfc;llen">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1436642962795" ID="ID_1765712063" MODIFIED="1436643069373" TEXT="Automations-L&#xfc;cken">
+<icon BUILTIN="flag-yellow"/>
+</node>
+<node CREATED="1436642920834" ID="ID_76129276" MODIFIED="1436643075203" TEXT="Pivot steuerbar machen">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1436642978642" ID="ID_1118009424" MODIFIED="1436643093128" TEXT="Scherung / Parallelogramm">
+<icon BUILTIN="licq"/>
+</node>
+<node CREATED="1436642933843" ID="ID_1292059741" MODIFIED="1436643096632" TEXT="D&#xe4;mpfungs-Filter">
+<icon BUILTIN="licq"/>
+</node>
+</node>
+</node>
+<node CREATED="1436642178566" HGAP="81" ID="ID_1345154609" MODIFIED="1436642184732" POSITION="right" TEXT="Integration" VSHIFT="96">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1436642185702" ID="ID_1881635323" MODIFIED="1436642191034" TEXT="lfd. Upstream">
+<node CREATED="1436642198422" ID="ID_63230369" MODIFIED="1436642220650" TEXT="2.70 &#x2023; 2.75a">
+<node CREATED="1436642231118" ID="ID_1478045690" MODIFIED="1436642242642" TEXT="BKE_tracking_marker_get_exact">
+<node CREATED="1436642264148" HGAP="25" ID="ID_507794779" MODIFIED="1436642284571" VSHIFT="-16">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      weitgehend ersetzt
+    </p>
+    <p>
+      durch BKE_tracking_marker_get
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1436642286181" ID="ID_365288615" MODIFIED="1436642460161" TEXT="Deutung: Versuch einer Interpolation">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ich erinnere mich, da&#223; das irgendwo in der Diskussion schon mal vorkam.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Wenn es eine L&#252;cke in den Tracking-Daten gibt, f&#228;llt der Stabiliser einfach aus.
+    </p>
+    <p>
+      Durch diese &#196;nderung f&#228;llt er stattdessen auf den n&#228;chsten Wert zur Linken (?) zur&#252;ck.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1436642298454" ID="ID_925480568" MODIFIED="1436642583543" TEXT="wer war das?">
+<icon BUILTIN="help"/>
+<node CREATED="1436642303742" ID="ID_768326846" MODIFIED="1436642337697" TEXT="das zeigt Interesse am Thema"/>
+<node CREATED="1436642597252" ID="ID_329769462" MODIFIED="1436642606358" TEXT="Vorischt Politik">
+<font NAME="SansSerif" SIZE="9"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1436642341461" ID="ID_1451582484" MODIFIED="1436642511553" TEXT="Bewertung: halbgar">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Das sind mir die typischen Schlaumeier-L&#246;sungen.
+    </p>
+    <p>
+      Wir basteln uns das hin!
+    </p>
+    <p>
+      Aber blo&#223; nicht ein Problem bei der Wurzel packen, das w&#228;re uncool.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1436642517285" ID="ID_412832600" MODIFIED="1436642578965" TEXT="Urteil: durch meine L&#xf6;sung ersetzen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ich habe eine lineare Interpolation implementiert, die sich wirklich symmetrisch verh&#228;lt
+    </p>
+    <p>
+      und auch all die komplexen Randf&#228;lle korrekt handhabt.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1436643251354" ID="ID_1278770990" MODIFIED="1436643256546" TEXT="calculate_autoscale_factor">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1436643258978" HGAP="24" ID="ID_1534897468" MODIFIED="1436643369246" VSHIFT="13">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      if (!stabilization_median_point_get(tracking, cfra, median)) {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;continue;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1436643306177" ID="ID_1635942159" MODIFIED="1436643325528">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      nun bedingungslos
+    </p>
+    <p>
+      und ohne Guard
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1436643327049" ID="ID_177311182" MODIFIED="1436643336197" TEXT="Grund: vermutlich marker_get_exact"/>
+</node>
+<node CREATED="1436643347425" ID="ID_1070614877" MODIFIED="1436643357954" TEXT="Vorsicht: setzt die alte Logik voraus">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1436642648667" ID="ID_1919094305" MODIFIED="1436642657080" TEXT="Problem: Format f&#xfc;r Daten-Feed"/>
+<node CREATED="1436642631660" ID="ID_923860681" MODIFIED="1436642639336" TEXT="Problem: Compositor-Nodes"/>
+</node>
+</node>
+</map>
